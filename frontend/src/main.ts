24a7c93fbcf65b6838cd91aee15f4377dd6a5c04
Alex Gustafsson <alex-gustafsson@live.se>
2021-06-02 19:14:49 +0200

    Implement basic client-side validation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,16 +11,56 @@ function disableBodyDragAndDrop() {
   });
 }
 
+function parseParameters() {
+  const fragments = location.hash.substr(1).split("&").reduce((fragments, fragment) => {
+    const [key, value] = fragment.split("=");
+    fragments[key] = value;
+    return fragments;
+  }, {})
+
+  return {token: fragments["token"] || null, secret: fragments["secret"] || null};
+}
+
+function parseClaims(token: string) {
+  const claims = JSON.parse(atob(token.split(".")[1]));
+  return {
+    maximumFileCount: claims["mfc"] || 0,
+    maximumFileSize: claims["mfs"] || 0,
+    maximumSize: claims["ms"] || 0,
+    expiresAt: new Date(claims["exp"] * 1000),
+    id: claims["jti"],
+    issuer: claims["iss"],
+  };
+}
+
 async function main() {
-  disableBodyDragAndDrop();
+  let { token, secret } = parseParameters();
+  if (token === null) {
+    // TODO: Show a in-page notification instead
+    alert("Unauthorized: No token present");
+    return;
+  }
+
+  const claims = parseClaims(token);
 
   const main = document.getElementById("main");
 
-  const dropper = new Dropper(document.getElementById("dropper"));
+  const dropper = new Dropper(document.getElementById("dropper"), {
+    maximumFileCount: claims.maximumFileCount,
+    maximumFileSize: claims.maximumFileSize,
+    maximumSize: claims.maximumSize,
+  });
   const fileList = document.getElementById("files");
   const fileScroller = document.getElementById("file-scroller");
 
+  disableBodyDragAndDrop();
+
   dropper.addEventListener("drop", (files: File[]) => {
+    if (secret === null)
+      secret = prompt("Secret");
+
+    // Allow 3.5 dropped files to be shown (makes it easier to see that the
+    // content may be scrolled once it surpasses four files
     main.style.height = `${400 + 70 * Math.min(dropper.files.length, 3.5)}px`;
     for (const file of files) {
       const droppedFile = new DroppedFile(file);
--- frontend/src/ui/dropper.ts
+++ frontend/src/ui/dropper.ts
@@ -1,13 +1,21 @@
 type DropperEventHandler = (Any?) => void;
 
+type DropperOptions = {
+  maximumFileCount: number,
+  maximumFileSize: number,
+  maximumSize: number,
+};
+
 export default class Dropper {
   private element: Element;
+  private options: DropperOptions
   private listeners: { [key: string]: DropperEventHandler[] };
 
   public files: File[];
 
-  constructor(element: Element) {
+  constructor(element: Element, options: DropperOptions) {
     this.element = element;
+    this.options = options;
     this.listeners = {};
 
     this.files = [];
@@ -23,6 +31,31 @@ export default class Dropper {
     event.cancelBubble = true;
 
     const droppedFiles = Array.from(event.dataTransfer.files);
+
+    if (this.options.maximumFileCount > 0) {
+      if (this.files.length + droppedFiles.length > this.options.maximumFileCount) {
+        alert(`Too many files, may at most upload ${this.options.maximumFileCount}`);
+        return;
+      }
+    }
+
+    if (this.options.maximumSize > 0) {
+      const currentSize = this.files.reduce((sum, file) => sum + file.size, 0);
+      const droppedSize = droppedFiles.reduce((sum, file) => sum + file.size, 0);
+      if (currentSize + droppedSize > this.options.maximumSize) {
+        alert(`Files are too large, may at most be ${this.options.maximumSize}B`);
+        return;
+      }
+    }
+
+    if (this.options.maximumFileSize > 0) {
+      const largeFiles = droppedFiles.filter(file => file.size > this.options.maximumFileSize);
+      if (largeFiles.length > 0) {
+        alert(`One or more files are too large: ${largeFiles.join(", ")}`);
+        return;
+      }
+    }
+
     this.files.push(...droppedFiles);
     for (const handler of this.listeners["drop"] || [])
       handler(droppedFiles);
