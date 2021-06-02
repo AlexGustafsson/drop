b3f5eac6f70c637c0c23609864ae33e8a3a8ca84
Alex Gustafsson <alex-gustafsson@live.se>
2021-06-02 12:25:28 +0200

    Watch CSS and EJS files

--- frontend/watch.ts
+++ frontend/watch.ts
@@ -3,10 +3,50 @@ import { renderFile } from "ejs";
 import postcss from "postcss";
 import atImport from "postcss-import";
 import cssnano from "cssnano";
-import { rm, readFile, writeFile, readdir } from "fs/promises";
+import chokidar from "chokidar";
+import { rm, readFile, writeFile, mkdir } from "fs/promises";
 
-async function main() {
-  await rm("dist", { force: true, recursive: true });
+async function buildHTML() {
+  const result = await renderFile(
+    "./src/index.ejs",
+    {
+      stylePath: `/static/main.css`,
+      scriptPath: `/static/main.js`,
+    },
+    {}
+  );
+
+  await writeFile("./dist/index.html", result);
+}
+
+async function buildCSS() {
+  const css = await readFile("./src/main.css");
+  const styleResult = await postcss()
+    .use(atImport())
+    .use(cssnano())
+    .process(css, {
+      from: "src/main.css",
+      to: "dist/static/main.css",
+      map: {
+        inline: false,
+      },
+    });
+
+  await writeFile(`./dist/static/main.css`, styleResult.css);
+  await writeFile(`./dist/static/main.css.map`, styleResult.map.toString());
+}
+
+async function buildTypeScript(once = false) {
+  const watchConfig = {
+    async onRebuild(error) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      await buildHTML();
+    },
+  };
 
   await build({
     entryPoints: ["src/main.ts"],
@@ -15,45 +55,32 @@ async function main() {
     sourcemap: true,
     outdir: "dist/static",
     tsconfig: "tsconfig.browser.json",
-    watch: {
-      async onRebuild(error) {
-        if (error) {
-          console.error(error);
-          return;
-        }
-
-        const files: [string] = await readdir("./dist/static");
-        console.log(files);
-        const mainScriptPath = files.find(x => x === "main.js");
-
-        const css = await readFile("./src/main.css");
-        const styleResult = await postcss()
-          .use(atImport())
-          .use(cssnano())
-          .process(css, {
-            from: "src/main.css",
-            to: "dist/static/main.css",
-            map: {
-              inline: false,
-            },
-          });
-
-        await writeFile(`./dist/static/main.css`, styleResult.css);
-        await writeFile(`./dist/static/main.css.map`, styleResult.map.toString());
-
-        const result = await renderFile(
-          "./src/index.ejs",
-          {
-            stylePath: `/static/main.css`,
-            scriptPath: `/static/${mainScriptPath}`,
-          },
-          {}
-        );
-
-        writeFile("./dist/index.html", result);
-      },
-    },
+    watch: once ? {} : watchConfig,
   });
+}
+
+async function onChange(path: string) {
+  if (path.endsWith(".ejs"))
+    buildHTML();
+  else if (path.endsWith(".css"))
+    buildCSS();
+}
+
+async function main() {
+  await rm("dist", { force: true, recursive: true });
+  await mkdir("dist");
+
+  buildTypeScript(true);
+  buildCSS();
+  buildHTML();
+
+  const watcher = chokidar.watch("./src", {persistent: true});
+
+  watcher.on("add", onChange);
+  watcher.on("change", onChange);
+  watcher.on("unlink", onChange);
+
+  buildTypeScript();
 }
 
 try {
