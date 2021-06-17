379689765274ea8e9a6908357c29570308bbb6d5
Alex Gustafsson <alex-gustafsson@live.se>
2021-06-17 08:29:37 +0200

    Remove comments

--- frontend/src/components/modal.tsx
+++ frontend/src/components/modal.tsx
@@ -27,16 +27,3 @@ export default class Modal extends React.Component<ModalProps> {
     </div >
   }
 }
-
-// position: fixed;
-// top: 0;
-// left: 0;
-// width: 100 %;
-// height: 100 %;
-// background - color: rgba(0, 0, 0, 0.3);
-// z - index: 1000;
-// backdrop - filter: blur(2px);
-// display: flex;
-// flex - direction: column;
-// align - items: center;
-// justify - content: center;
