5177c0905f711d9b2c92ded0f2b991f8b77ae3a4
Alex Gustafsson <alex-gustafsson@live.se>
2021-09-12 12:11:56 +0200

    Fix file icon size

--- frontend/src/components/dropped-file.tsx
+++ frontend/src/components/dropped-file.tsx
@@ -9,8 +9,8 @@ type DroppedFileProps = {
 };
 
 const DroppedFile = ({title, uploadProgress}: DroppedFileProps): JSX.Element => {
-  return <li className="grid grid-rows-2 grid-cols-triple h-16">
-    <FileIcon className="w-14 row-span-full animate-move-in animation-delay-2 text-primary" />
+  return <li className="grid grid-rows-2 grid-cols-triple h-16 items-center">
+    <FileIcon className="w-8 row-span-full animate-move-in animation-delay-2 text-primary" />
     <p className="col-start-2 animate-move-in animation-delay-4">{title}</p>
     <p className="col-start-3 animate-move-in text-right animation-delay-6">{Math.round(uploadProgress * 100)}%</p>
     <div className="relative row-span-2 col-start-2 col-span-2 bg-gray-50 h-3 w-full rounded-md animate-fade-in animation-delay-8">
