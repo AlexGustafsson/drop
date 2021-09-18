f713add9ae98d9f7df2422aa96d995b1b76496cc
Alex Gustafsson <alex-gustafsson@live.se>
2021-09-18 11:25:26 +0200

    Improve human readable durations

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,3 +1,10 @@
+const TIME_MINUTE = 60
+const TIME_HOUR = 60 * TIME_MINUTE
+const TIME_DAY = 24 * TIME_HOUR
+const TIME_WEEK = 7 * TIME_DAY
+const TIME_MONTH = 4 * TIME_WEEK
+const TIME_YEAR = 12 * TIME_MONTH
+
 export function humanReadableBytes(bytes: number, decimals = 2) {
   if (bytes === 0)
     return "0 B"
@@ -10,9 +17,30 @@ export function humanReadableBytes(bytes: number, decimals = 2) {
 }
 
 export function humanReadableDuration(timestamp: number) {
-  const now = new Date().getUTCSeconds();
-  const difference = now - timestamp;
-  let duration = `${difference} seconds`;
+  const now = Math.floor(new Date().valueOf() / 1000);
+  console.log(timestamp, now);
+  let difference = now - timestamp;
+  let duration = "";
+  if (difference >= TIME_YEAR) {
+    let years = difference / TIME_YEAR;
+    duration = `${years} years`;
+  } else if (difference > TIME_MONTH) {
+    let months = Math.round(difference / TIME_MONTH);
+    duration = `${months} months`;
+  } else if (difference >= TIME_DAY) {
+    let day = Math.round(difference / TIME_DAY);
+    duration = `${day} day`;
+  } else if (difference >= TIME_HOUR) {
+    let hour = Math.round(difference / TIME_HOUR);
+    duration = `${hour} hour`;
+  } else if (difference >= TIME_MINUTE) {
+    let minutes = Math.round(difference / TIME_MINUTE);
+    duration = `${minutes} minutes`;
+  } else if (difference >= 0) {
+    duration = `${difference} seconds`;
+  }
+
+  duration = duration.trimEnd();
   if (difference < 0)
     duration = "in " + duration;
   else
